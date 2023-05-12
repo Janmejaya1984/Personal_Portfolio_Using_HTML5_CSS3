7d5e561c045dd109eeca8288d26d9d9183f0074c
Janmejaya <bapun00@gmail.com>
2023-05-12 18:20:45 +0530

    added all files

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 const navbar = document.querySelector(".navbar");
-const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".navbar-link");
 const progressBarWrapper = document.querySelector(".progress-bar-wrapper");
-const progressPercentages = [97,67,85,77,54,99,67];
-const progressPercentageDivs = document.querySelectorAll(".progress-percent");
+const progressBarPercens = [97,67,85,77,54,99,67];
+const navbarOffsetTop = navbar.offsetTop;
+const progressPercentDivs = document.querySelectorAll(".progress-percent");
 window.addEventListener('scroll',()=>{
     if(window.pageYOffset>=navbarOffsetTop){
         navbar.classList.add('sticky');
     }else{
         navbar.classList.remove('sticky');
     }
+
     sections.forEach((section,i)=>{
-       if(window.pageYOffset >= section.offsetTop){
-        navbarLinks.forEach((navbarLink)=>{
-            navbarLink.classList.remove('change');
+      if(window.pageYOffset >= section.offsetTop){
+        navbarLinks.forEach((link)=>{
+          link.classList.remove("change");
         });
-        navbarLinks[i].classList.add('change');
-       }
+        navbarLinks[i].classList.add("change");
+      }
     });
-    if(window.pageYOffset+window.innerHeight >=progressBarWrapper.offsetTop){
-         progressPercentageDivs.forEach((element,i)=>{
-               element.style.width= `${progressPercentages[i]}%`;
-         });
+   
+    if(window.pageYOffset >= progressBarWrapper.offsetTop-200){
+        progressPercentDivs.forEach((element,i)=>{
+           element.style.width = `${progressBarPercens[i]}%`;
+        });
     }
+
 });
